85d031b061c95d315df499ad0e4ca77474c112ab
Maria del Mar Cegarra <maria.cegarra2303@gmail.com>
2022-05-26 18:48:22 +0200

    Changed type modules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   env: {
-    commonjs: true,
+    browser: true,
     es2021: true,
-    node: true,
   },
   extends: ["airbnb-base", "airbnb-typescript", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
+    sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
   rules: {},
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,2 @@
 const a = 3;
-console.log();
+console.log(a);
